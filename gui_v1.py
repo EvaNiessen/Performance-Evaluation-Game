d48577f861e263cd213a6b80f487e21c8a138883
EvaNiessen <83288397+EvaNiessen@users.noreply.github.com>
2021-05-10 15:59:28 +0200

    Add files via upload

---
+++ gui_v1.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import rules_v1
+from bildgenerator_v1 import Bildgenerator
+from psychopy import locale_setup, sound, gui, visual,event , core, data, logging, hardware, parallel, iohub
+import time
+import pyglet
+import sys
+from random import randint
+
+class Gui:
+    
+    '''Provides an userinterface and analyzes the userinputs'''
+    def __init__(self, input_keys, highscore,picture_size, evaluation_time, color_scheme):
+        self.evaluation_time = evaluation_time
+        self.picture_size = picture_size
+        self.keys = input_keys
+        self.win = visual.Window(
+            fullscr=True, screen=2,
+            allowGUI=True, allowStencil=False,
+            monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+            blendMode='avg', useFBO=True)
+        self.win.color = [-1,-1,-1]
+        self.win.flip()
+        self.score = 0 #zu Beginn 0 Punkte
+        self.highscore_anzeige = visual.TextStim(win = self.win, units='pix', pos=(0,2.5*self.picture_size), text = "Highscore: "+str(highscore), height = self.picture_size/4)
+        self.blocksleft = visual.TextStim(win = self.win, units='pix', pos=(0,1.5*self.picture_size), height = self.picture_size/4)
+        self.score_anzeige = visual.TextStim(win=self.win, units='pix', pos=(0,2.0*self.picture_size), height = self.picture_size/4)
+        self.win.mouseVisible=False
+        self.pictures = Bildgenerator(color_scheme)
+        self.mouse=event.Mouse(win=self.win)
+        self.image = []
+        self.image1 = visual.ImageStim(win=self.win, units='pix', pos=(0,self.picture_size), size=(self.picture_size*4,self.picture_size*2))
+        #Dient für das zeichnen der Zielbilder
+        self.shapes = []
+        self.text = visual.TextStim(win = self.win, units='pix', pos=(0,self.picture_size), height = self.picture_size/3)
+        self.text.wrapWidth = self.win.size[0]
+        #Dient für anzeigen von Texten wie z.B. "End of Game"
+        self.texts = []
+        self.create_textshapes_and_pictures()
+        
+    def create_textshapes_and_pictures(self):
+        '''Creates the textshape for highscore screen and the pictures which could be selected'''
+        for i in range(0,4):
+            self.shapes.append(visual.ShapeStim(win = self.win ,units = 'pix',
+                                                vertices = ((-self.picture_size/2,self.picture_size/2),(-self.picture_size/2,-self.picture_size/2),
+                                                (self.picture_size/2,-self.picture_size/2),(self.picture_size/2,self.picture_size/2)),
+                                                fillColor = [1,1,1], lineColor = [-1,1,-1]))
+            self.shapes[i].pos = (self.picture_size*(1.2*i-1.8),-self.picture_size)
+            #Generiert  vier gruene Vierecke mit der selben Position und Groesse wie die Auswahlbilder
+            self.texts.append(visual.TextStim(win = self.win,units = 'pix',pos=(self.picture_size*(1.2*i-1.8),-self.picture_size), color = [-1,-1,-1]))
+            #Generiert vier Textfelder mit der selben Position wie die Auswahlbilder
+            self.texts[i].height = self.picture_size/5
+            self.texts[i].wrapWidth = self.picture_size/2
+            #Passt die Schriftgroesse an die groesse aus dem configfile an
+            self.image.append(visual.ImageStim(win=self.win, units = 'pix', pos=(self.picture_size*(1.2*i-1.8),-self.picture_size), size=(self.picture_size,self.picture_size)))
+            self.image[i].setImage("Bilder/tmp/option"+str(i)+".jpg")
+            #Legt die Auswahlbilder fest
+        self.texts[0].text = "Ja"
+        self.texts[1].text = "Vielleicht ja"
+        self.texts[2].text = "Vielleicht nicht"
+        self.texts[3].text = "Nein"
+        
+    def show_pictures(self, trial):
+        aim_vertices =self.picture_size/10
+        aim = visual.ShapeStim(win = self.win ,units = 'pix',
+                               vertices = ((-aim_vertices,aim_vertices),(-aim_vertices,-aim_vertices),
+                               (aim_vertices,-aim_vertices),(aim_vertices,aim_vertices)),pos=(0,self.picture_size))
+        aim.draw()  #Zeichnet ein viereck an die Stelle der Bilder
+        for i in range(4):
+            self.image[i].opacity = 1
+            self.image[i].draw()
+        self.win.flip()
+        waittime = randint(6,10)
+        time.sleep(waittime/10.0)   #Zeige das viereck für 0.6 bis 1 Sekunde
+        self.pictures.set_target(trial[0], trial[1])
+        self.image1.setImage("Bilder/tmp/target.jpg")
+        self.image1.draw()
+        for i in range(0,4):
+            self.image[i].opacity = 1
+            self.image[i].draw()
+        self.win.flip()
+        
+    def show_right(self, choosen):
+        '''Shows the correct and the chosen picture for introduction Block; after this the self-evaluation screen is shown'''
+        for i in range(4):
+            if i == self.pictures.position:
+                self.shapes[i].lineColor = (-1,1,-1)        #Umrandet das richtige Bild mit einem gruenen Kasten
+                self.shapes[i].lineWidth = 5
+                self.shapes[i].draw()
+            elif i==choosen:
+                self.shapes[i].lineColor = (1,-1,-1)        #Umrandet das gewaehlte Bild mit einem roten Kasten
+                self.shapes[i].lineWidth = 5
+                self.shapes[i].draw()
+            else:
+                self.image[i].opacity = 0.5                 #Alle anderen Bilder werden ausgeblasst
+            self.image[i].draw()
+        self.image1.draw()
+        self.win.flip()
+        event.waitKeys(keyList = self.keys)
+            
+    
+    def show_intertrial(self, choosen):
+        '''Returns the button which is pressed in the self-evaluation screen, returns -1 if no key is pressed'''
+        for i in range(4):
+            if i==choosen:
+                self.shapes[i].lineColor = (0.1,0.9,1)        #Umrandet das gewaehlte Bild mit einem tuerkis blauen Kasten
+                self.shapes[i].lineWidth = 5
+                self.shapes[i].draw()
+            else:
+                self.image[i].opacity = 0.3                 #Alle anderen Bilder werden ausgeblasst
+            self.image[i].draw()
+        self.win.flip()
+        time.sleep(0.7)
+        self.win.flip()
+        time.sleep(1)
+        self.text.setText("War das gewaehlte Bild richtig?")
+        self.text.color = [1,1,1]
+        self.text.draw()
+        for i in range(0,4):
+            self.shapes[i].lineColor = (0.1,0.9,1)  # farbe anders
+            self.shapes[i].lineWidth = 3
+            self.shapes[i].draw()
+            self.texts[i].draw()
+        self.win.flip()
+        key_input = []
+        event.getKeys()                                         #Leert den Eingabebuffer
+        starttime2 = time.time()
+        key_input = event.waitKeys(maxWait = self.evaluation_time, keyList = self.keys)
+        if key_input:
+            return (self.keys.index(key_input[0]),round((time.time()-starttime2)*1000, 5))
+        return (-1, 0)                                               #Returnt -1 falls keine Eingabe stattfindet
+
+
+    def show_timeout(self):
+        self.text.setText("TIMEOUT!")
+        self.text.color = [1,-1,-1]
+        self.text.draw()
+        
+        
+    def show_introduction(self):
+        '''Asks whether another introduction block should be started'''
+        self.text.setText("Erneuter Durchgang der Einleitung?")
+        self.text.draw()
+        self.texts[1].text = "Ja" # Setzt den Text zweier Evaluationsbuttons auf 'yes' und 'no'
+        self.texts[2].text = "Nein"  
+        for i in range(1,3):
+            self.shapes[i].lineColor = (0.1,0.9,1) # farbe anders
+            self.shapes[i].lineWidth = 3
+            self.shapes[i].draw()
+            self.texts[i].draw()
+        self.win.flip()
+        key_input = event.waitKeys(keyList = ['y', 'n'])
+        self.texts[1].text = "Vielleicht ja" #Setzt den Text zurück auf die Evaluationstexte
+        self.texts[2].text = "Vielleicht nicht"
+        return key_input[0] == 'y'
+        
+    def show_tryout(self):
+        '''Asks whether another tryout block should be started'''
+        self.text.setText("Erneuter Durchgang des Trainings?")
+        self.text.draw()
+        self.texts[1].text = "Ja" # Setzt den Text zweier Evaluationsbuttons auf 'yes' und 'no'
+        self.texts[2].text = "Nein"         
+        for i in range(1,3):
+            self.shapes[i].lineColor = (0.1,0.9,1) # farbe anders
+            self.shapes[i].lineWidth = 3
+            self.shapes[i].draw()
+            self.texts[i].draw()
+        self.win.flip()
+        key_input = event.waitKeys(keyList = ['y', 'n'])
+        self.texts[1].text = "Vielleicht ja" #Setzt den Text zurück auf die Evaluationstexte
+        self.texts[2].text = "Vielleicht nicht"
+        return key_input[0] == 'y'
+        
+        
+    def show_end_of_game(self, max):
+        '''Shows the score and the highscore at the end of game'''
+        self.score_anzeige.setText("Score: "+str(self.score)+ " / "+str(max))
+        self.text.setText("Ende des Spiels")
+        self.text.draw()
+        self.score_anzeige.draw()
+        self.highscore_anzeige.draw()
+        self.win.flip()
+        event.waitKeys(keyList = self.keys)
+        
+
+    def show_begin_of_block(self, pause, max, counter):
+        ''' Shows the  highscore, own score and the maximum possible score at the beginning of each block, 
+        pause is the minimum time a participant has to wait before they can start the next block'''
+        for i in range(pause):
+            self.score_anzeige.setText("Score: "+str(self.score)+ " / "+str(max))
+            self.score_anzeige.draw()
+            self.blocksleft.setText("Verbleibende Durchgaenge: "+str(counter))
+            self.blocksleft.draw()
+            self.highscore_anzeige.draw()
+            self.text.setText("Weiter in:"+str(pause-i)+" Sekunden")
+            self.text.draw()
+            self.win.flip()
+            time.sleep(1)
+        self.score_anzeige.setText("Score: "+str(self.score)+ " / "+str(max))
+        self.score_anzeige.draw()
+        self.blocksleft.setText("Verbleibende Durchgaenge: "+str(counter))
+        self.blocksleft.draw()
+        self.highscore_anzeige.draw()
+        self.text.setText("Druecken Sie eine Taste um weiter zu machen")
+        self.text.draw()
+        self.win.flip()
+        event.waitKeys(keyList = self.keys)
+        aim_vertices =  self.picture_size/10
+        aim = visual.ShapeStim(win = self.win ,units = 'pix',
+                               vertices = ((-aim_vertices,aim_vertices),(-aim_vertices,-aim_vertices),
+                               (aim_vertices,-aim_vertices),(aim_vertices,aim_vertices)),pos=(0,self.picture_size))
+        aim.draw()                          #Zeichnet ein kleines Viereck an die Stelle des Bilder
+        self.win.flip()
+        time.sleep(1)
+
+    
+    def which_button(self, stoptime):
+        '''Returns the button which is pressed while Gamescreen'''
+        event.getKeys()         #Leert den Eingabebuffer
+        starttime = time.time()
+        key_input = event.waitKeys(maxWait = stoptime, keyList = self.keys)
+        if key_input:
+            return (self.keys.index(key_input[0]),round((time.time()-starttime)*1000, 5))   #returnt (Den eingebenen Button, Die benötigte Zeit)
+        return (-1, -1)  #Sollte keine Eingabe erfolgen wird fuer Zeit und Button -1 zurueck gegeben
+        
+    def show_rules(self):
+        ''' Shows an introduction to the rules and the experiment'''
+        text_display = visual.TextStim(self.win, units = 'pix', pos = (0, -1.5*self.picture_size), height = self.picture_size/5)
+        image_display = visual.ImageStim(self.win, units = 'pix', pos=(0, 1.5*self.picture_size),
+                                size = (self.picture_size*6, self.picture_size*3.5))
+        #Erzeugen von Text und Bildfeld zum anzeigen der Einfuehrung
+        while rules_v1.content:
+            text_display.setText(rules_v1.content[0][0])   #Setzt den Text auf den als erstes im Tupel angegebenen String
+            image_display.setImage(rules_v1.content[0][1]) #Setzt das Bild auf die als zweites im Tupel angegebene Quellldatei
+            del rules_v1.content[0]
+            text_display.draw()
+            image_display.draw()
+            self.win.flip()
+            event.waitKeys(keyList = self.keys)
+            
---
+++ main_v1.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy2 Experiment Builder (v1.85.3),
+    on Do 30 Aug 2018 10:16:54 CEST
+If you publish work using this script please cite the PsychoPy publications:
+    Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
+        Journal of Neuroscience Methods, 162(1-2), 8-13.
+    Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy.
+        Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
+"""
+
+from gui_v1 import Gui
+import random, configparser, cv2, time
+
+name = 'Demo'
+
+def generate_block(block_length, congruent_quantity):
+    '''Preset which rules and which pictures are shown during a block'''
+    block = []
+    congruent = random.sample(range(block_length),congruent_quantity) #Zieht die Stellen des Blockes an welchem die congruent rule verwendet wird
+    pool = range(block_length)
+    picture = [[],[],[],[]]
+    for i in range(4):                                                #Fuer jedes Bild
+        for j in range((block_length/4)+1):                           #Wird (Blocklaenge/4) mal
+            if (4*(j+1)) <= block_length or i<block_length % 4:       #Wenn noch "Positionen uebrig" sind
+                picture[i].append(random.choice(pool))                #Zufaellig eine Position gezogen
+                pool.remove(picture[i][j])
+    for i in range(block_length):                                     #Fuer jede Stelle des Blockes wird
+        next_trial = []                                               #Wird nun ein Wertepaar erzeug
+        if i in congruent:                                            #Fuer den ersten Wert wird nur geprueft ob die Stelle als congruent gezogen wird
+            next_trial.append(1)
+        else :
+            next_trial.append(2)
+        if i in picture[0]:                                           #Fuer den zwei6ten Wert wird geprueft zu welchem Bild die Stelle des Blockes gehoert
+            next_trial.append(0)
+        elif i in picture[1]:
+            next_trial.append(1)
+        elif i in picture[2]:
+            next_trial.append(2)
+        elif i in picture[3]:
+            next_trial.append(3)
+        block.append(next_trial)
+    return block
+
+
+def introduction(gui, configs):
+    '''first practise without timelimit and feedback, no info in logfile'''
+    pool = [0,0,1,1,2,2,3,3]
+    block_procedure = generate_block(int(configs['Block']['tryout_length']), int(configs['Block']['congruent_number']))
+    for j in range(int(configs['Block']['tryout_length'])):
+        gui.show_pictures(block_procedure[j])
+        guessed_number=gui.which_button('inf')
+        choosen = guessed_number[0]
+        evaluation = gui.show_right(choosen)
+        
+
+def tryout(gui, configs):
+    '''second practise identical with real experiment, but without timeout adjustment, no info in logfile'''
+    pool = [0,0,1,1,2,2,3,3]
+    block_procedure = generate_block(int(configs['Block']['tryout_length']), int(configs['Block']['congruent_number']))
+    for j in range(int(configs['Block']['tryout_length'])):
+        gui.show_pictures(block_procedure[j])
+        guessed_number=gui.which_button(float(configs['General']['timeout']))
+        choosen = guessed_number[0]
+        if guessed_number[0] == -1:
+            if not pool:
+                pool = [0,0,1,1,2,2,3,3]
+            choosen = random.choice(pool)
+            pool.remove(choosen)
+            gui.show_timeout()
+        evaluation = gui.show_intertrial(choosen)
+
+def main():
+    max = 0
+    ID = name
+    rules =  configparser.ConfigParser()
+    configs = configparser.ConfigParser()
+    rules.read('rules.cfg')
+    configs.read('settings.cfg')
+    timeout = float(configs['General']['timeout'])
+    gui = Gui(  str(configs['General']['input_keys']).replace(" ","").split(','), 
+                int(configs['General']['highscore']),
+                int(configs['General']['picture_size']), 
+                float(configs['General']['evaluation_time']),
+                int(configs['General']['color_scheme']))
+    gui.show_rules()
+    while gui.show_introduction():
+        introduction(gui, configs)
+    while gui.show_tryout():
+        tryout(gui, configs)
+    starttime = time.time()
+    logfile = open("logfile_{0}.txt".format(ID), 'w')
+    logfile2 = open("logfile2_{0}.txt".format(ID), 'w')
+    logfile2.write("block \t trial_nr \t trial_type \t RT \t miss \t chosen_button \t correct_button \t evaluation \t accuracy \t RT_eval \t category_left \t color_left \t category_right \t color_right \r\n")
+    for i in range(int(configs['General']['block_number'])):
+        pool = [0,0,1,1,2,2,3,3]                                #Ist der Imagepool fuer CPU-Auswahl bei Timeout am anfang jedes Blockes aufgefuellt
+        block_procedure = generate_block(int(configs['Block']['main_length']), int(configs['Block']['congruent_number_main']))
+        gui.show_begin_of_block(int(configs['Block']['pause']), max, int(configs['General']['block_number']) - i)
+        guessed_right = 0
+        for j in range(int(configs['Block']['main_length'])):
+            gui.show_pictures(block_procedure[j])
+            guessed_number=gui.which_button(timeout)
+            choosen = guessed_number[0]
+            if guessed_number[0] == gui.pictures.position:      #Wenn das geratene Bild richtig war
+                gui.score += 10                                 #Erhoehe Score um 10
+                guessed_right += 1                              #Und zaehle die richtigen Bilder hoch
+            if guessed_number[1] == -1:                         #Sollte kein Bild gewaehlt worden sein wird eins aus dem imagepool gezogen
+                if not pool:                                    #Sollte der Pool leer sein wird dieser neu aufgefuellt
+                    pool = [0,0,1,1,2,2,3,3]
+                choosen = random.choice(pool)
+                pool.remove(choosen)
+                gui.show_timeout()
+            evaluation = gui.show_intertrial(choosen)
+            eval_button = evaluation[0] #Auswahl der Evaluation
+            eval_time = evaluation[1] #RT der Evaluation
+            
+            if((evaluation[0] < 2 and choosen == block_procedure[j][1]) or (evaluation[0] > 2 and choosen != block_procedure[j][1])): #Wenn die Selsteinschaetzung richtig
+                    gui.score += 3                                                                                              #erhoehe Score um 3
+            if guessed_number[1] > -1: #Info fuer logfile wenn Antwort gegeben wurde
+                logfile.write("{0} \t {1} \t  {2} \t {3:04.0f} \t 0 \t {4} \t {5} \t {6} \t {7} \t {8:04.0f} \t {9} \t {10} \t {11} \t {12} \r\n".format(
+                i+1,j+1,block_procedure[j][0],guessed_number[1], guessed_number[0]+1,gui.pictures.position+1, 4-eval_button, int(guessed_number[0]==gui.pictures.position),eval_time,
+                gui.pictures.last_shown_images[0][0]+1, gui.pictures.last_shown_images[0][1]+1,gui.pictures.last_shown_images[1][0]+1, gui.pictures.last_shown_images[1][1]+1 ))
+            else: #Info fuer logfile bei timeout
+                logfile.write("{0} \t {1} \t {2} \t 0 \t 1 \t {3} \t {4} \t {5} \t 3 \t {6:04.0f} \t {7} \t {8} \t {9} \t {10}\r\n".format(
+                i+1,j+1,block_procedure[j][0],choosen+1,gui.pictures.position+1,4-eval_button, eval_time,
+                gui.pictures.last_shown_images[0][0]+1, gui.pictures.last_shown_images[0][1]+1,gui.pictures.last_shown_images[1][0]+1, gui.pictures.last_shown_images[1][1]+1))
+            if not ((j+1)%(int(configs['Block']['main_length'])/2)): #Wenn die Haelfte eines Blockes erreicht ist
+                if (2*guessed_right/float(configs['Block']['main_length']) < float(configs['Block']['increase_timeout'])): #Und die richtige anzahl kleiner der vorgegebenen Prozentzahl ist
+                    timeout *= 1.1                                                                                    #Erhoehe timeout um 10%
+                elif(2*guessed_right/float(configs['Block']['main_length']) > float(configs['Block']['decrease_timeout'])): #Und die richtige anzahl kleiner der vorgegebenen Prozentzahl ist
+                    timeout *= 0.9                                                                                     #Verringere timeout um 10%
+                guessed_right = 0
+        max += 13*int(configs['Block']['main_length']) #Pro Versuch koennen zehn Punkte durch Bilder und drei durch Bewertung erreicht werden
+        logfile2.write("Time for block{0}: {1} \r\n".format(i+1, round(time.time()-starttime,3)))
+        logfile2.write("Current timeout: {0} \r\n".format(timeout))
+    logfile2.write("\r\n \r\n \r\n \r\n")
+    logfile2.write("Score: {0}\n".format(gui.score))
+    logfile2.write("Color Schema: {0}\n".format(int(configs['General']['color_scheme'])))
+    logfile2.write("trial: \t 1) congruent rule \t 2) incongruent rule \r\n")
+    logfile2.write("evaluation: \t 1) probably not \t 2) maybe not \t 3) maybe yes \t 4) probably yes 5) no evaluation \r\n") 
+    logfile2.write("miss: \t 0) no miss \t 1) timeout ")
+    logfile.close()
+    logfile2.close()
+    gui.show_end_of_game(max)
+
+if __name__=="__main__":
+    main()
+    
