0b2e73cd1e53ab76f0db06fc38e8ffcabd84882c
EvaNiessen <83288397+EvaNiessen@users.noreply.github.com>
2022-03-21 15:26:00 +0100

    Add files via upload

---
+++ bildgenerator_v2.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from psychopy import visual
+import imutils
+import numpy as np
+from random import randint, choice
+import cv2 as cv
+import time
+import os
+
+class Bildgenerator:
+
+    def __init__(self, color_scheme):
+        if color_scheme == 1:                           #Setzt die decode_tabelle der Farbcodes:
+            self.color_decode = [0,1,2,3]               #Die 1. Zahl im Array gibt die Position des blauen Bildes an
+        elif color_scheme == 2:                         #Die 2. Zahl im Array gibt die Position des gruenen Bildes an
+            self.color_decode = [3,2,1,0]               #Die 3. Zahl im Array gibt die Position des roten Bildes an
+        elif color_scheme == 3:                         #Die 4. Zahl im Array gibt die Position des gelben Bildes an
+            self.color_decode = [2,0,3,1]
+        elif color_scheme == 4:
+            self.color_decode = [1,3,0,2]
+        self.last_shown_images = [[], []]               #Speichert die letzen beiden gezeigten Bilder in zwei Listen
+                                                        #Dabei enthaelt eine Liste zwei Eintraege: Einen Integer fuer Form und einen fuer Farbe
+        self.position = 0
+        self.image_pool = [[],[],[],[]]
+        self.initialize_image_pool()
+        self.set_option()
+
+    def initialize_image_pool(self):
+        image_pool =[[],[],[],[]]
+        for i in range(0,4):
+            for j in range(1,9):
+                if i == 0:
+                    self.image_pool[0].append("Ball"+str(j))
+                elif i == 1:
+                    self.image_pool[1].append("Eis"+str(j))
+                elif i == 2:
+                    self.image_pool[2].append("Stuhl"+str(j))
+                elif i == 3:
+                    self.image_pool[3].append("Vogel"+str(j))
+
+    def set_option(self):
+        '''Creates the answer option pictures and writes them to Bilder/tmp'''
+        for i in range(0,4):
+            img = cv.imread("Bilder/"+self.image_pool[i][0]+".jpg",cv.IMREAD_COLOR)
+            img = self.colorize_img(img, i)
+            reduction = 10
+            img = img.astype(np.float32)
+            img = cv.resize(img, (190,190), interpolation = cv.INTER_AREA)
+            img = cv.copyMakeBorder(img, reduction, reduction, reduction, reduction, cv.BORDER_CONSTANT, value=(255,255,255))
+            if not os.path.exists("Bilder/tmp"):
+                os.makedirs("Bilder/tmp")
+            cv.imwrite("Bilder/tmp/option"+str(i)+".jpg", img)
+
+    def set_image1(self,form, color):
+        image1 = []
+        image1.append(form)
+        image1.append(color)
+        self.last_shown_images[0] = image1
+
+    def set_image2(self,form, color):
+        image2 = []
+        image2.append(form)
+        image2.append(color)
+        self.last_shown_images[1] = image2
+
+    def swap_images(self):
+        self.last_shown_images[0], self.last_shown_images[1] = self.last_shown_images[1], self.last_shown_images[0]
+
+    def concatenate_second_img(self, img1, form_pool, color_pool):
+        form=choice(form_pool)                          #Waehlt zufaellig eine Form aus
+        art=randint(1,len(self.image_pool[form])-1)
+        img2 = cv.imread("Bilder/"+self.image_pool[form][art]+".jpg", cv.IMREAD_COLOR)
+        color_pool.remove(form)                        #Entfernt die zugehörige Farbe der Form, damit das zweite Bild keines der unteren matcht
+        color = choice(color_pool)
+        img2 = self.colorize_img(img2, color) #Faerbt das Bild random mit einer der uebrig gebliebenden Farben
+        img2 = Bildgenerator.rotate_img(img2)
+
+        self.set_image2(form, color)
+        if randint(0,1):
+            img1 = np.concatenate((img1, img2), axis=1)
+        else:
+            self.swap_images()
+            img1 = np.concatenate((img2, img1), axis=1)
+        cv.imwrite("Bilder/tmp/target.jpg", img1)
+
+    def set_target(self, rule, picture):
+        '''Selects randomly which method is used to set the target, with lower level the pictures will more often be exactly like
+        one picture(instead of missmatching one picture)'''
+        self.position = picture
+        if rule == 1:
+            self.set_target1()
+            return 1
+        elif rule==2:
+            self.set_target2()
+            return 2
+
+    def set_target2(self):
+        '''Generates one target picture with two pictures(only one picture is completly dissmatching)'''
+        pool = [0, 1, 2, 3]
+        pool.remove(self.position)
+        form1 = choice(pool)        #Zufaelliges Auswaehlen zweier Formen,
+        pool.remove(form1)          #welche nicht zu der Antwort passen
+        form2 = choice(pool)        #und entfernen aus dem pool,
+        pool.remove(form2)          #damit das Bild nicht die selbe Form und Farbe hat wie eines der Antwortbilder
+        color1 = pool[0]            #Weist die letzte Farbe dem ersten Bild zu
+        color2 = form1              #Und die Farbe der Form des ersten Bildes dem zweiten Bild
+        art=randint(1,len(self.image_pool[form1])-1)
+        target1 = cv.imread("Bilder/"+self.image_pool[form1][art]+".jpg")
+        target1 = self.colorize_img(target1, color1)
+        art=randint(1,len(self.image_pool[form2])-1)
+        target2 = cv.imread("Bilder/"+self.image_pool[form2][art]+".jpg")
+        target2 = self.colorize_img(target2, color2)
+        target2 = Bildgenerator.reduce_img_size(target2)
+        target1 = Bildgenerator.reduce_img_size(target1)
+        target2 = Bildgenerator.rotate_img(target2)
+        target1 = Bildgenerator.rotate_img(target1)
+        self.set_image1(form1, color1)
+        self.set_image2(form2, color2)
+        if randint(0,1):
+            target1 = np.concatenate((target1, target2), axis=1) #Fuegt die Bilder an der vertikalen Achse zusammen
+        else:
+            self.swap_images()
+            target1 = np.concatenate((target2, target1), axis=1)
+        cv.imwrite("Bilder/tmp/target.jpg", target1)
+
+    def set_target1(self):
+        '''Generates one target picture with two pictures(only one picture is completly matching)'''
+        color_pool = [0, 1, 2, 3]
+        form_pool = [0, 1, 2, 3]
+        art=randint(1,len(self.image_pool[self.position])-1)      #Waehlt zufaellig eine Art der gegeben Form aus
+        form_pool.remove(self.position)
+        target = cv.imread("Bilder/"+self.image_pool[self.position][art]+".jpg",cv.IMREAD_COLOR)
+        color_pool.remove(self.position)
+        self.set_image1(self.position, self.position)
+        target = self.colorize_img(target,self.position) #Da der Farbcode der Optionbilder aufsteigend durchnummeriert wird
+        target = Bildgenerator.rotate_img(target)
+        self.concatenate_second_img(target, form_pool, color_pool) #Fuegt die Bilder an der vertikalen Achse zusammen
+
+    def colorize_img(self, img, color_code):
+        '''Colorize the image to the given color code'''
+        #Invertiert alle Bilder und setzt dann den entsprechenden Farbkanal auf null und invertiert das Bild dann wieder
+        img = ((255,255,255)-img)
+        img[:,:,:] = img//128                       #Setzt alle pixel die invertiert ueber 128 liegen auf 1 alle anderen auf 0, somit werden alle Werte die im Bild dunkel waren 1 alle hellen 0
+        if color_code == self.color_decode[0]:                      #Blau
+            img[:,:,0] *= (255-207)              #(255-RGB-Werte, da diese am ende noch einmal invertiert werden
+            img[:,:,1] *= (255-82)
+            img[:,:,2] *= (255-21)
+        elif color_code ==  self.color_decode[1]:                    #Gruen
+            img[:,:,0] *= (255-27)              #(255-RGB-Werte, da diese am ende noch einmal invertiert werden
+            img[:,:,1] *= (255-179)
+            img[:,:,2] *= (255-42)
+        elif color_code ==  self.color_decode[2]:                    #Rot
+            img[:,:,0] *= (255-39)              #(255-RGB-Werte, da diese am ende noch einmal invertiert werden
+            img[:,:,1] *= (255-11)
+            img[:,:,2] *= (255-222)
+        elif color_code ==  self.color_decode[3]:                    #Gelb
+            img[:,:,0] *= (255-16)                 #(255-RGB-Werte, da diese am ende noch einmal invertiert werden
+            img[:,:,1] *= (255-226)
+            img[:,:,2] *= (255-242)
+        img = ((255,255,255)-img)
+        return img
+
+    @staticmethod
+    def rotate_img(img):
+        '''Returns the given picture rotated by a random degree between -90 and 90 degrees'''
+        img = img.astype(np.float32)
+        img = ((255,255,255)-img)
+        img = imutils.rotate_bound(img,randint(-40,40))
+        img = cv.resize(img, (285,285))                     #sqrt(2)*200(groesst moegliche laenge bei 200 pixel grossen bildern)
+        img = ((255,255,255)-img)
+        return img
+
+    @staticmethod
+    def reduce_img_size(img):
+        '''Returns a random reduced image of the given picture'''
+        reduction = randint(10,60)
+        dim = ((200-reduction*2), (200-reduction*2))
+        img = img.astype(np.float32)
+        img = cv.resize(img, dim, interpolation = cv.INTER_AREA)    #Reduziert das auf die gegebene Dimension
+        img = cv.copyMakeBorder(img, reduction, reduction, reduction, reduction, cv.BORDER_CONSTANT, value=(255,255,255)) #Und fuegt dann einen Rand hinzu
+        return img
--- rules_v1.py
+++ rules_v1.py
@@ -1,34 +1,34 @@
-content = [ ("Herzlich Willkommen zu unserem Experiment!", "Bilder/Rules/PEG.png"),
+content = [ ("Herzlich Willkommen zu unserem Experiment!", "Bilder/Rules/SIG2.png"),
 
-	("Ihre Aufgabe wird es sein, so schnell und so richtig wie moeglich auf gezeigte Bilder zu reagieren. Bitte nutzen Sie im gesamten Experiment die Tasten der rechten und linken Zeige- und Mittelfinger.", "Bilder/Rules/PEG.png"),
+	("Ihre Aufgabe wird es sein, so schnell und so richtig wie möglich auf gezeigte Bilder zu reagieren. Bitte nutzen Sie im gesamten Experiment die Tasten der rechten und linken Zeige- und Mittelfinger.", "Bilder/Rules/SIG2.png"),
 
-	("Vier Kategorien von Zielen werden dabei die ganze Zeit unveraendert sichtbar sein: ein Ball, ein Eis, ein Sessel und ein Vogel. Jedem Ziel ist dabei eine bestimmte Farbe zugeordnet. Oben sehen Sie eine beispielhafte Zuordnung.", "Bilder/Rules/Ziele.png"),
+	("Vier Kategorien von Zielen werden dabei die ganze Zeit unverändert sichtbar sein: ein Ball, ein Eis, ein Sessel und ein Vogel. Jedem Ziel ist dabei eine bestimmte Farbe zugeordnet. Oben sehen Sie eine beispielhafte Zuordnung.", "Bilder/Rules/Ziele.png"),
 
-	("Zudem erscheint bei jedem Durchgang ein Bild ueber den Zielen, welches immer aus zwei Komponenten besteht: Zwei Exemplare der Zielkategorien in unterschiedlichen Farben. Diese Bilder entscheiden, welches Ziel in dem jeweiligen Durchgang richtig ist.", "Bilder/Rules/Beispiel_Durchgang.png"),
+	("Zudem erscheint bei jedem Durchgang ein Bild über den Zielen, welches immer aus zwei Komponenten besteht: Zwei Exemplare der Zielkategorien in unterschiedlichen Farben. Diese Bilder entscheiden, welches Ziel in dem jeweiligen Durchgang richtig ist.", "Bilder/Rules/Beispiel_Durchgang.png"),
 
-	("Die Bilder sind nie identisch mit den Zielen, wichtig ist die dazugehoerige Kategorie. Das bedeutet zum Beispiel, dass jeder Vogel als Vogel behandelt wird, egal wie er dargestellt ist. Hier sehen Sie alle Voegel, die wir zeigen werden.", "Bilder/Rules/Alle_Vogel.png"),
+	("Die Bilder sind nie identisch mit den Zielen, wichtig ist die dazugehörige Kategorie. Das bedeutet zum Beispiel, dass jeder Vogel als Vogel behandelt wird, egal wie er dargestellt ist. Hier sehen Sie alle Vögel, die wir zeigen werden.", "Bilder/Rules/Alle_Vogel.png"),
 
 	("Hier sehen Sie alle Eis, die wir zeigen werden. Sehen Sie sich alle in Ruhe an.", "Bilder/Rules/Alle_Eis.png"),
 
-	("Hier sehen Sie alle Baelle, die wir zeigen werden. Sehen Sie sich alle in Ruhe an.", "Bilder/Rules/Alle_Ball.png"),
+	("Hier sehen Sie alle Bälle, die wir zeigen werden. Sehen Sie sich alle in Ruhe an.", "Bilder/Rules/Alle_Ball.png"),
 
-	("Hier sehen Sie alle Stuehle, die wir zeigen werden. Sehen Sie sich alles in Ruhe an.", "Bilder/Rules/Alle_Stuhl.png"),
+	("Hier sehen Sie alle Stühle, die wir zeigen werden. Sehen Sie sich alles in Ruhe an.", "Bilder/Rules/Alle_Stuhl.png"),
 
-	("Um zu entscheiden, welches Ziel richtig ist, gibt es zwei zu beachtende Regeln. Die erste Regel ist einfach: Sollte eines der vier unteren Ziele genau mit einem der beiden gezeigten Bilder (Kategorie und Farbe) uebereinstimmen, so ist dieses auszuwaehlen. Im Beispiel: blauer Ball", "Bilder/Rules/Bildschirmfoto2.png"),
+	("Um zu entscheiden, welches Ziel richtig ist, gibt es zwei zu beachtende Regeln. Die erste Regel ist einfach: Sollte eines der vier unteren Ziele genau mit einem der beiden gezeigten Bilder (Kategorie und Farbe) übereinstimmen, so ist dieses auszuwählen. Im Beispiel: blauer Ball", "Bilder/Rules/Bildschirmfoto2.png"),
 
-	("Die zweite Regel ist etwas schwieriger: Sollte keins der vier unteren Bilder mit einem der gezeigten Bilder uebereinstimmen, so ist das Ziel auszuwaehlen, welches weder als Kategorie noch als Farbe in dem gezeigten Bild vorkommt. Im Beispiel: gruenes Eis", "Bilder/Rules/Bildschirmfoto3.png"),
+	("Die zweite Regel ist etwas schwieriger: Sollte keins der vier unteren Bilder mit einem der gezeigten Bilder übereinstimmen, so ist das Ziel auszuwählen, welches weder als Kategorie noch als Farbe in dem gezeigten Bild vorkommt. Im Beispiel: grünes Eis", "Bilder/Rules/Bildschirmfoto3.png"),
 
-	("Bitte beachten Sie: Sie haben fuer Ihre Antwort nur begrenzt Zeit. Sollten Sie diese Zeit ueberschreiten, wird Ihnen dies durch die Anzeige \"Timeout\" symbolisiert. Im Falle einer Zeitueberschreitung waehlt der Computer ein zufaelliges Ziel fuer Sie aus.","Bilder/Rules/Bildschirmfoto4.png"),
+	("Bitte beachten Sie: Sie haben für Ihre Antwort nur begrenzt Zeit. Sollten Sie diese Zeit überschreiten, wird Ihnen dies durch die Anzeige \"Timeout\" symbolisiert. Im Falle einer Zeitüberschreitung wählt der Computer ein zufälliges Ziel für Sie aus.","Bilder/Rules/Bildschirmfoto4.png"),
 
 	("Nach der Auswahl eines Zieles (egal ob durch Sie oder den Computer) sollen Sie die Auswahl beurteilen. Dabei koennen Sie unsere vierstufige Skala nutzen (wieder mit ihren rechten und linken Zeige- und Mittelfingern).", "Bilder/Rules/Bildschirmfoto5.png"),
 
-	("Um Ihre Leistung zu belohnen, gibt es ein Punktesystem: Fuer jede richtige Antwort erhalten Sie 10 Punkte, und eine richtige Einschaetzung der Antwort wird mit 3 Punkten belohnt.", "Bilder/Rules/Punktesystem.png"),
+	("Um Ihre Leistung zu belohnen, gibt es ein Punktesystem: Für jede richtige Antwort erhalten Sie 10 Punkte, und eine richtige Einschätzung der Antwort wird mit 3 Punkten belohnt.", "Bilder/Rules/Punktesystem.png"),
 
-	("Im Laufe des Experiments wird es immer wieder Pausen geben, deren Dauer Sie selbst bestimmen koennen. In diesen Pausen werden Ihnen ihre aktuellen Punkte (im Verhaeltnis zu den moeglichen Punkten) sowie ein Highscore gezeigt.", "Bilder/Rules/Bildschirmfoto6.png"),
+	("Im Laufe des Experiments wird es immer wieder Pausen geben, deren Dauer Sie selbst bestimmen koennen. In diesen Pausen werden Ihnen ihre aktuellen Punkte (im Verhältnis zu den möglichen Punkten) sowie ein Highscore gezeigt.", "Bilder/Rules/Bildschirmfoto6.png"),
 
-	("Sollten Sie es schaffen, unseren Highscore zu knacken, wartet eine Ueberraschung auf Sie!", "Bilder/Rules/Bildschirmfoto6.png"),
+	("Sollten Sie es schaffen, unseren Highscore zu knacken, wartet eine Überraschung auf Sie!", "Bilder/Rules/Bildschirmfoto6.png"),
 
-	("Wenn Sie keine Fragen mehr haben, starten wir zuerst mit einem Training, bevor das eigentliche Experiment losgeht. Viel Erfolg!", "Bilder/Rules/PEG.png")]
+	("Wenn Sie keine Fragen mehr haben, starten wir zuerst mit einer Übung und einem Training, bevor das eigentliche Experiment losgeht. Viel Erfolg!", "Bilder/Rules/SIG2.png")]
 	
 
 
